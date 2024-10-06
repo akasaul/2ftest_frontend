32202ca1a2546b46656bec4a8c17f6ba98a5ef1b
Nikodimos Jemaneh <nikodimosjemaneh40@gmail.com>
2024-10-06 12:55:09 +0300

    fix: build fixes

--- actions/registerRestaurant.ts
+++ actions/registerRestaurant.ts
@@ -5,7 +5,6 @@ import { signUpRestaurantSchema } from "@/schmas/auth";
 export const SignUpRestaurant = async (data: FormData) => {
   const safeData = signUpRestaurantSchema.safeParse(data);
   if (!safeData.success) throw new Error("Invalid data");
-  const { login, email, password, profileImage } = safeData.data;
 
   try {
   } catch (error: any) {
