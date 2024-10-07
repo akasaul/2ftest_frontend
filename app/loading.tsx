160511be7f9459ec271a732281f58cc4453c9ec7
Nikodimos Jemaneh <nikodimosjemaneh40@gmail.com>
2024-10-07 09:31:22 +0300

    fix: build fixes

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import PizzaLoader from "@/components/ui/Loader";
+import { Box } from "@mui/material";
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>Loading..</div>
-  )
-}
+    <Box sx={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: '100%'
+    }}>
+      <PizzaLoader />
+    </Box>
+  );
+};
 
-export default Loading
+export default Loading;
